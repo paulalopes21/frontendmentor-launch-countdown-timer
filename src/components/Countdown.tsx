6dfdef300338bfeef1f8160e94921a2fb65e2267
Paula Lopes <pls21@msn.com>
2021-02-28 22:14:37 -0300

    Countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,26 +1,60 @@
-import { render } from 'react-dom';
+import { useEffect, useState } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
+function calculateTimeLeft() {
+  const difference = +new Date('2021,06,21 23:59:59') - +new Date();
+  let timeLeft = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60)
+    };
+  }
+
+  return timeLeft;
+}
+
 export default function Countdown() {
-  return(
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => {
+      clearTimeout(id);
+    };
+  });
+ 
+   return(
     <div className={styles.countdownContainer}>
       <div>
-        <span>08</span>
+         <span>{timeLeft['days']}</span>
         <span>days</span>
       </div>
 
       <div>
-        <span>23</span>
+         <span>{timeLeft['hours']}</span>
         <span>hours</span>
       </div>
       
       <div>
-        <span>55</span>
+         <span>{timeLeft['minutes']}</span>
         <span>minutes</span>
       </div>
       
       <div>
-        <span>41</span>
+         <span>{timeLeft['seconds']}</span>
         <span>seconds</span>
       </div>
     </div>
